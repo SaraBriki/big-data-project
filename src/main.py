1bd1ad401693b48ac85a39c0f0913890cfd3472f
Sara Briki <sarabriki@insat.u-carthage.tn>
2023-05-10 19:41:55 +0100

    last edits

--- src/main.py
+++ src/main.py
@@ -15,69 +15,57 @@ from dotenv import load_dotenv
 
 def prints(s):
     print(f'[X] {s}')
+
+
 def image_input(inp):
-
-    # Prepare model input
-
+    # Prepare model inpu
     resized_image_rgb = inp.convert('RGB')
     prints('image converted')
     resized_image = resized_image_rgb.resize((224, 224))
     prints('image resized')
     input_nparr = np.array(resized_image)
+
     # model_input = tf.reshape(input_nparr, [1, 224, 224, 3])
     final_input = tf.keras.applications.resnet50.preprocess_input(input_nparr)
     prints('image preprocessed')
-    final_input = final_input[None,: ]
+    final_input = final_input[None, :]
 
     # Instanciate model
-
     avg_pool = tf.keras.layers.GlobalAveragePooling2D()
 
     # Feed input to model and predict
     print(final_input.shape)
     emb = avg_pool(model(final_input))
     prints('inference completed')
+
     # Query top 3 nearest embeddings
     print('[*] sending query')
     query_results = index.query(emb.numpy().tolist(), top_k=3)
     prints('query successful')
+
     # Save query results to global variable
     from pathlib import Path
     dataset_path = Path('full_dataset')
-    results=[]
+    results = []
     for r in query_results['matches']:
-        filename = r['id']+".png"
-        file_path = dataset_path/filename
+        filename = r['id'] + ".png"
+        file_path = dataset_path / filename
         results.append(file_path)
         print(file_path)
-    print("yay user input is here!")
     return results
-
-
-# def generate_gallery():
-#     images = [
-#         (str(results[i]), f"Image result {i + 1}")
-#         for i in range(3)
-#     ]
-#     print(images)
-#     return images
 
 
 with gr.Blocks() as demo:
     with gr.Column(variant="panel"):
         with gr.Row():
             image = gr.Image(type="pil")
-            # image.change(fn=image_input, inputs=image, outputs=None)
         with gr.Row():
             btn = gr.Button("Search image").style(full_width=True)
         with gr.Row():
-            # gallery = gr.Gallery(
-            #     label="Result images", show_label=False, elem_id="gallery"
-            # ).style(columns=3, rows=1, object_fit="contain", height="auto")
             out1 = gr.Image(type="filepath")
             out2 = gr.Image(type="filepath")
             out3 = gr.Image(type="filepath")
-            btn.click(image_input, image, outputs=[out1,out2,out3])
+            btn.click(image_input, image, outputs=[out1, out2, out3])
 
 # Connect to pinecone environment
 load_dotenv()
